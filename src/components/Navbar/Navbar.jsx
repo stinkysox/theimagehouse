1ca3a627c3a3093d9ea14b288bb2e5fea1b9a3a5
Hevit <kopelahevit5@gmail.com>
2025-08-29 12:34:33 +0530

    phone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,50 +7,40 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <section className="navbar">
-      <div className="navbar-one">
-        <motion.img
-          src="https://i.postimg.cc/L6VwNNDP/LOGO-Clubbed-WHITE.png"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="navbar-logo"
-        />
-      </div>
-
-      {/* Social icons */}
-      <div className="navbar-socials">
-        <motion.div
-          className="nav-social-media-icon instagram"
-          initial={{ opacity: 0, x: 30 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-          whileHover={{
-            rotate: [0, -10, 10, -10, 10, 0],
-            transition: { duration: 0.4 },
-          }}
+      <motion.img
+        src="https://i.postimg.cc/L6VwNNDP/LOGO-Clubbed-WHITE.png"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        className="navbar-logo"
+      />
+      <motion.a
+        href="tel:+919876543210"
+        className="navbar-phone"
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+      >
+        +91 98765 43210
+      </motion.a>
+      <motion.div
+        className="nav-social-media-icon instagram"
+        initial={{ opacity: 0, x: 30 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+        whileHover={{
+          rotate: [0, -10, 10, -10, 10, 0],
+          transition: { duration: 0.4 },
+        }}
+      >
+        <a
+          href="https://www.instagram.com/theimageryhouse/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <a
-            href="https://www.instagram.com/theimageryhouse/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IoLogoInstagram />
-          </a>
-        </motion.div>
-
-        <motion.div
-          className="nav-social-media-icon facebook"
-          initial={{ opacity: 0, x: 30 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6, delay: 0.6 }}
-          whileHover={{
-            rotate: [0, -10, 10, -10, 10, 0],
-            transition: { duration: 0.4 },
-          }}
-        >
-          {/* Facebook icon functionality can go here */}
-        </motion.div>
-      </div>
+          <IoLogoInstagram />
+        </a>
+      </motion.div>
     </section>
   );
 };
